17448654736ec92aa01fc019c27f879ce3526c4b
saaamoli <sara.mol.1995@gmail.com>
2025-01-08 18:58:50 +0100

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -24,7 +24,7 @@ def resolve_dataset_path(args, dataset_name):
     if dataset_name_lower == "dtd":
         return os.path.join(base_path, "dtd")
     elif dataset_name_lower == "eurosat":
-        return os.path.join(base_path, "EuroSAT_splits")
+        return base_path 
     elif dataset_name_lower == "mnist":
         return os.path.join(base_path, "MNIST", "raw")
     elif dataset_name_lower == "gtsrb":
