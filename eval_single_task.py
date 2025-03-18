50fc4f32dafb38c3e9a2ca7d2e25b7fc2caf44e1
AMIRHOSSEIN MOSLEMI <amirmoslemi88.am@gmail.com>
2025-03-18 18:29:39 +0100

    Update eval_single_task.py lr to baseline

--- eval_single_task.py
+++ eval_single_task.py
@@ -197,10 +197,10 @@ def main():
     args = parse_arguments()
 
     # Define necessary paths
-    args.checkpoints_path = "/kaggle/working/checkpoints_lr"
-    args.results_dir = "/kaggle/working/results_lr"
+    args.checkpoints_path = "/kaggle/working/checkpoints_baseline"
+    args.results_dir = "/kaggle/working/results_baseline"
     args.data_location = "/kaggle/working/datasets"
-    args.save = "/kaggle/working/checkpoints_lr"
+    args.save = "/kaggle/working/checkpoints_baseline"
 
     # List of datasets to evaluate
     datasets = ["DTD", "EuroSAT", "GTSRB", "MNIST", "RESISC45", "SVHN"]
