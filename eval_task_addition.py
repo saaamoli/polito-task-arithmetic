831ae7b9753226105b4d2518dc2525a6a6c28337
saaamoli <sara.mol.1995@gmail.com>
2025-04-02 10:19:24 +0200

    Update eval_task_addition.py

--- eval_task_addition.py
+++ eval_task_addition.py
@@ -223,10 +223,10 @@ def evaluate_multitask_metrics(args, encoder, task_vectors, datasets, alpha, tra
 def main():
     # Parse arguments
     args = parse_arguments()
-    args.checkpoints_path = "/kaggle/working/checkpoints_baseline"
+    args.checkpoints_path = "/kaggle/working/checkpoints_batchsize"
     args.data_location = "/kaggle/working/datasets"
-    args.results_dir = "/kaggle/working/results_baseline"
-    args.save = "/kaggle/working/checkpoints_baseline"
+    args.results_dir = "/kaggle/working/results_batchsize"
+    args.save = "/kaggle/working/checkpoints_batchsize"
 
     # Define the list of datasets
     datasets = ["DTD", "EuroSAT", "GTSRB", "MNIST", "RESISC45", "SVHN"]
