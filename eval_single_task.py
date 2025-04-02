9fa2141692864080d72ae375d06957b2b2afb2e9
saaamoli <sara.mol.1995@gmail.com>
2025-04-02 10:17:59 +0200

    Update eval_single_task.py

--- eval_single_task.py
+++ eval_single_task.py
@@ -197,10 +197,10 @@ def main():
     args = parse_arguments()
 
     # Define necessary paths
-    args.checkpoints_path = "/kaggle/working/checkpoints_baseline"
-    args.results_dir = "/kaggle/working/results_baseline"
+    args.checkpoints_path = "/kaggle/working/checkpoints_batchsize"
+    args.results_dir = "/kaggle/working/results_batchsize"
     args.data_location = "/kaggle/working/datasets"
-    args.save = "/kaggle/working/checkpoints_baseline"
+    args.save = "/kaggle/working/checkpoints_batchsize"
 
     # List of datasets to evaluate
     datasets = ["DTD", "EuroSAT", "GTSRB", "MNIST", "RESISC45", "SVHN"]
