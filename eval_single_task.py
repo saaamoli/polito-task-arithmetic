0a26255954cd43cd2722a1a89ee1cf17c5783add
saaamoli <sara.mol.1995@gmail.com>
2025-05-04 12:04:48 +0200

    Update eval_single_task.py weight

--- eval_single_task.py
+++ eval_single_task.py
@@ -197,10 +197,10 @@ def main():
     args = parse_arguments()
 
     # Define necessary paths
-    args.checkpoints_path = "/kaggle/working/checkpoints_lr"
-    args.results_dir = "/kaggle/working/results_lr"
+    args.checkpoints_path = "/kaggle/working/checkpoints_weight"
+    args.results_dir = "/kaggle/working/results_weight"
     args.data_location = "/kaggle/working/datasets"
-    args.save = "/kaggle/working/checkpoints_lr"
+    args.save = "/kaggle/working/checkpoints_weight"
 
     # List of datasets to evaluate
     datasets = ["DTD", "EuroSAT", "GTSRB", "MNIST", "RESISC45", "SVHN"]
