03928bb331ebc4af710df0f0675ccd9bcef76b85
saaamoli <sara.mol.1995@gmail.com>
2025-05-10 15:43:36 +0200

    Update base.sh

--- launch_scripts/base.sh
+++ launch_scripts/base.sh
@@ -1,21 +1,18 @@
 # 1) finetune on all datasets
 # 2) eval single task
 # 3) task addition
-pwd
-ls -la
 
-python /kaggle/working/polito-task-arithmetic/finetune.py \
---data-location=/kaggle/working/datasets/ \
---save=/kaggle/working/checkpoints/ \
+python finetune.py \
+--data-location=/path/to/datasets/ \
+--save=/path/to/save/ \
 --batch-size=32 \
 --lr=1e-4 \
 --wd=0.0
 
+python eval_single_task.py \
+--data-location=/path/to/datasets/ \
+--save=/path/to/save/
 
-#python eval_single_task.py \
-#--data-location=/content/drive/MyDrive/Task_Arithmetic_Datasets/datasets/ \
-#--save=/content/drive/MyDrive/Task_Arithmetic_Checkpoints/
-
-#python eval_task_addition.py \
-#--data-location=/content/drive/MyDrive/Task_Arithmetic_Datasets/datasets/ \
-#--save=/content/drive/MyDrive/Task_Arithmetic_Checkpoints/
+python eval_task_addition.py \
+--data-location=/path/to/datasets/ \
+--save=/path/to/save/
